202b328a482b3f9cb7600823546d2128f7422db7
Julian Zapata Vidal <julianzapatavidal@gmail.com>
2023-08-30 16:40:44 -0300

    Tarea de objetos resuelta

--- Clases/Alumnos.java
+++ Clases/Alumnos.java
@@ -1,17 +1,21 @@
 package Clases;
 
 public class Alumnos extends Persona{
-    private int[] nota = new int[3];
+    private final int[] nota = new int[3];
 
     public Alumnos(){}
-    public Alumnos(String apyn,int n1,int n2,int n3){super(apyn);this.nota[1]=n1;this.nota[2]=n2;this.nota[3]=n3;}
+    public Alumnos(String apyn,int n1,int n2,int n3) {
+        super(apyn);
+        this.nota[0]=n1;
+        this.nota[1]=n2;
+        this.nota[2]=n3;}
 
     //setters
-    public void setNota1(int n1){this.nota[1]=n1;}
-    public void setNota2(int n2){this.nota[2]=n2;}
-    public void setNota3(int n3){this.nota[3]=n3;}
+    public void setNota1(int n1){this.nota[0]=n1;}
+    public void setNota2(int n2){this.nota[1]=n2;}
+    public void setNota3(int n3){this.nota[2]=n3;}
     //getter
-    public int getNota1(int n1){return this.nota[1]=n1;}
-    public int getNota2(int n2){return this.nota[2]=n2;}
-    public int getNota3(int n3){return this.nota[3]=n3;} 
+    public int getNota1(){return this.nota[0];}
+    public int getNota2(){return this.nota[1];}
+    public int getNota3(){return this.nota[2];}
 }
--- Principal.java
+++ Principal.java
@@ -9,20 +9,44 @@ public class Principal {
         Scanner sc = new Scanner(System.in);
         ArrayList<Alumnos> lista = new ArrayList<>();
         Alumnos chango = new Alumnos();
-        int seleccion = 0;
+        int seleccion;
         String nombre;
-        int nota;
+        int nota1,nota2,nota3;
+        String busqueda;
+
 
         do {
             poner("Seleccione la opcion deseada");
             poner("1.- Ingresar alumno");
-            poner("2.- Leer alumnos");
+            poner("2.- Leer notas de los alumnos");
+            seleccion = sc.nextInt();
             switch(seleccion){
                 case 1:
                     poner("Ingrese nombre del alumno");
                     nombre = sc.next();
+                    poner("Ingrese la primera nota");
+                    nota1 = sc.nextInt();
+                    poner("Ingrese la segunda nota");
+                    nota2 = sc.nextInt();
+                    poner("Ingrese la tercera nota");
+                    nota3 = sc.nextInt();
+                    lista.add(new Alumnos(nombre,nota1,nota2,nota3));
                     break;
                 case 2:
+                    poner("Ingrese el nombre del alumno a buscar");
+                    busqueda = sc.next();
+                    poner(busqueda);
+                    for (Alumnos changuito : lista){
+                        String auxBusqueda = changuito.getNombre();
+                        if (auxBusqueda.equals(busqueda)){
+                            poner("************************************" +
+                                    "\n Nombre: " + auxBusqueda +
+                                    "\n Nota 1: " + changuito.getNota1() +
+                                    "\t Nota 2: " + changuito.getNota2() +
+                                    "\t Nota 3: " + changuito.getNota3() +
+                                    "\n ************************************");
+                        }
+                    }
                     break;
                 default:
                     break;
